1c985bcc2d2ffb962796303caf1d7d1b797312c1
Steven Will <stevegoblue09@gmail.com>
2025-07-07 15:26:00 -0400

    Simplify component to just display variable value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { client, SigmaClientProvider, useVariable } from '@sigmacomputing/plugin'
 
 // Configure the variable
@@ -10,28 +10,14 @@ client.config.configureEditorPanel([
 ])
 
 function TestComponent() {
-  const [inputValue, setInputValue] = useState('')
-  const [value, setValue] = useVariable('test-variable')
+  const [value] = useVariable('test-variable')
   
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    setValue(inputValue)
-  }
-
   return (
     <div style={{ padding: '20px' }}>
       <h3>Test Sigma Plugin</h3>
-      <div style={{ marginBottom: '10px' }}>
-        Current Sigma value: {value || 'none'}
+      <div>
+        Variable value: {value || 'none'}
       </div>
-      <form onSubmit={handleSubmit}>
-        <input 
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          placeholder="Enter a value"
-        />
-        <button type="submit">Update Sigma Variable</button>
-      </form>
     </div>
   )
 }
