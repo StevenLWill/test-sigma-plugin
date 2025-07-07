d84c0c3c846cb097891776bb8191c0f71483dea6
Steven Will <stevegoblue09@gmail.com>
2025-07-07 15:36:59 -0400

    Add debugging for variable value

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { client, SigmaClientProvider, useVariable } from '@sigmacomputing/plugin'
 
 // Configure the variable
@@ -12,11 +12,19 @@ client.config.configureEditorPanel([
 function TestComponent() {
   const [value] = useVariable('test-variable')
   
+  // Debug the value
+  useEffect(() => {
+    console.log('Variable value:', value)
+  }, [value])
+
   return (
     <div style={{ padding: '20px' }}>
       <h3>Test Sigma Plugin</h3>
       <div>
         Variable value: {value || 'none'}
+      </div>
+      <div style={{ color: '#666', fontSize: '12px', marginTop: '10px' }}>
+        Debug info: type={typeof value}, value={JSON.stringify(value)}
       </div>
     </div>
   )
